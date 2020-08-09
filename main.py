dd9b2bdb7eb3376b8909a23c2294025fedbd18f2
Nolan Kovacik <noltron000@gmail.com>
2020-08-09 13:14:02 -0400

    [doc] main.py comments

--- main.py
+++ main.py
@@ -6,16 +6,17 @@ from markdown_maker import generate_descriptions
 
 
 if __name__ == '__main__':
-	# all features, ever
+	# Obtain all features, ever.
 	all_features \
 		= collect_features()
 
-	# categorized class features
+	# Categorized class features.
 	class_features \
 		= compose_class_features(all_features)
 
-	# useful progression table index for classes
+	# Create a useful progression table index for classes.
 	class_progressions \
 		= compose_class_progressions(class_features)
 
+	# Ensure every feature has a description for every class.
 	generate_descriptions(all_features)
