481c90d314e30431c4696f51fe444ee7989fd8da
Nolan Kovacik <noltron000@gmail.com>
2020-08-20 13:23:19 -0400

    [add] variables.js organizational file

--- request-data.js
+++ request-data.js
@@ -1,23 +1,27 @@
-import {githubAccessToken as token} from './env.js'
-
-/* USE A MODULE */
+/* OBTAIN CODE IMPORTS */
 // Using HTML modules, we'll import this project:
 // https://github.com/octokit/graphql.js/
 import {graphql} from 'https://cdn.pika.dev/@octokit/graphql'
+// These static variables are neatly stored in another file.
+import {
+	fetchInit as graphqlInit,
+	endpoint,
+	account,
+	project,
+	branch,
+	version,
+} from './variables.js'
 
-/* PREPARE FOR REQUEST */
-// We'll need three things for our request:
-// - the GraphQL query that we want called
-// - our GitHub Access Token
-// - the Authorization Header, with our token
-
+/* PREPARE FOR GRAPHQL REQUEST */
+// This GraphQL query is where the magic happens.
+// Intead of hitting multiple endpoints, there is only one!
 // If you'd like to mess around with the GitHub API, visit the GraphiQL playground here:
 // https://developer.github.com/v4/explorer/
 const query = `
 query GetFiles {
-	organization(login: "mythril-forge") {
-		repository(name: "character-data") {
-			object(expression: "master:source/homebrew/abilities/features") {
+	organization(login: "${account}") {
+		repository(name: "${project}") {
+			object(expression: "${branch}:source/${version}/abilities/features") {
 				... on Tree {
 					entries {
 						name
@@ -34,34 +38,32 @@ query GetFiles {
 }
 `
 
-// Get your github access token at:
-// https://github.com/settings/tokens
-const authorization = {
-	headers: {
-		authorization: 'token ' + token
-	}
-}
-
-/* CREATE THE FUNCTION */
+/* CREATE THE DATA COLLECTOR */
 // Create an async function to make the request.
-const getFileData = async (query, authorization) => {
-	// The the request is made when graphql is called.
-	const result = await graphql(query, authorization)
+const getFileData = async () => {
+	// The request is made when graphql is called.
+	const result = await graphql(query, graphqlInit)
 	const entries = await result.organization.repository.object.entries
+
+	// The resulting object is over-structured for my purpose, so I'm flattening it a bit.
 	// Loop through all the entries to consolidate the data.
 	const files = {}
 	for (const entry of entries) {
+
 		const entryName = entry.name
 		const entryText = entry.object.text
+		// Some directories may exist in the results, but we don't need them.
+		// To avoid adding those uneeded folders, ignore files that don't have any text.
 		if (entryText !== undefined) {
 			files[entryName] = entryText
 		}
 	}
+
 	// Return the files in a Promise.
 	return files
 }
 
-/* STEP 4: USE THE FUNCTION */
+/* MAKE MODULE EXPORTS */
 // This will resolve the promise and print it to console.
 // You can expand the objects and subobjects to see data.
-export const fileData = getFileData(query, authorization)
+export default getFileData()
