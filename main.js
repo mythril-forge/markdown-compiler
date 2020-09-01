62e183d9c1176c1e48a6deaeb42376d9c2baa7a6
Nolan Kovacik <noltron000@gmail.com>
2020-09-01 08:33:38 -0400

    [upd] testing new tools in main.js

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import {
 	groupByClasses,
 	groupByName,
 	filterByClass,
+	fillProgression,
 } from './parse-data.js'
 
 
@@ -16,15 +17,35 @@ import {
 	const features = prepareFeatureData(featureData)
 	// const classes = prepareClassData(classData)
 
+	// Categorized class features.
 	const featuresByClass = features.reduce(groupByClasses(), {})
 	const featuresByName = features.reduce(groupByName(), {})
-	const featuresForRogue = features.filter(filterByClass('rogue'))
-	// console.dir(getClassProgression(featuresByClass))
-	// Ensure every feature has a description for every class.
-
-	// Categorized class features.
+	const className = 'cleric'
+	const featuresForClass = features.filter(filterByClass(className))
 
 	// Create a useful progression table index for classes.
+	const featureProgression = features.reduce(fillProgression(className), [
+		{Level: 1, Features: []},
+		{Level: 2, Features: []},
+		{Level: 3, Features: []},
+		{Level: 4, Features: []},
+		{Level: 5, Features: []},
+		{Level: 6, Features: []},
+		{Level: 7, Features: []},
+		{Level: 8, Features: []},
+		{Level: 9, Features: []},
+		{Level: 10, Features: []},
+		{Level: 11, Features: []},
+		{Level: 12, Features: []},
+		{Level: 13, Features: []},
+		{Level: 14, Features: []},
+		{Level: 15, Features: []},
+		{Level: 16, Features: []},
+		{Level: 17, Features: []},
+		{Level: 18, Features: []},
+		{Level: 19, Features: []},
+		{Level: 20, Features: []},
+	])
 
 	// Create a summary for all the classes.
 
@@ -35,5 +56,7 @@ import {
 	console.dir(features)
 	console.dir(featuresByName)
 	console.info('grouped:')
-	console.dir(featuresForRogue)
+	console.dir(featuresForClass)
+	console.info('progression:')
+	console.dir(featureProgression)
 })()
