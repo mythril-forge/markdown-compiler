810657770014c0baabdc8e434cd3f2760c9cc8ee
Nolan Kovacik <noltron000@gmail.com>
2020-12-10 15:13:52 -0500

    [upd] mvp user interface

--- main.js
+++ main.js
@@ -43,24 +43,68 @@ const hookFetchData = async () => {
 
 	// Obtain option-group items.
 	for (const classItem of classes) {
-		console.log(classItem['slug'])
-		selectionHTML += '\n'
-		selectionHTML += `\t<option value=${classItem['slug']}>${classItem['name']}</option>`
+		selectionHTML += `\t<option value=${classItem['slug']}>${classItem['name']}</option>\n`
 	}
-	selectionHTML += '\n'
 
 	// Apply option-group items to selection input.
 	document
 	.querySelector('#class-selection select')
 	.innerHTML = selectionHTML
 
+	// Hook up event listener
+	document
+	.getElementById('class-selection')
+	.addEventListener('input', hookLevelFields)
+
 	// Unhide class selection fields.
 	document
 	.getElementById('class-selection')
 	.removeAttribute('hidden')
+}
 
-	/* EXPERIMENTATION BELOW */
-	/***************************************************************************************
+const hookLevelFields = () => {
+	const checkedNodes = document
+	.querySelectorAll('#class-selection select option:checked')
+
+	let levelingHTML = ''
+
+	// Obtain option-group nodes.
+	for (const node of checkedNodes) {
+		const classSlug = node.value
+		const classInfo = classes.find(classItem => classItem.slug === classSlug)
+		levelingHTML += '\t<div>\n'
+		levelingHTML += `\t\t<label for='${classInfo.slug}-levels'>${classInfo.name} Levels</label>\n`
+		levelingHTML += `\t\t<div id=${classInfo.slug}-levels data-class='${classInfo.slug}'>`
+		levelingHTML += `\t\t\t<input class='first-level' type='number' />\n`
+		levelingHTML += `\t\t\t<input class='final-level' type='number' />\n`
+		levelingHTML += `\t\t</div>`
+		levelingHTML += '\t</div>\n'
+	}
+
+	// Apply class-level group items to the input array.
+	document
+	.getElementById('class-leveling')
+	.innerHTML = levelingHTML
+
+	// Hook up event listener
+	document
+	.querySelector('#describe-progression input')
+	.addEventListener('click', hookWriteDescription)
+
+	// Hide or unhide describe-progression button.
+	if (levelingHTML) {
+		document
+		.getElementById('describe-progression')
+		.removeAttribute('hidden')
+	}
+	else {
+		document
+		.getElementById('describe-progression')
+		.setAttribute('hidden', true)
+	}
+}
+
+const hookWriteDescription = () => {
 	// Categorized class features.
 	const featuresByName = features
 	.reduce(groupByName(), {})
@@ -68,15 +112,27 @@ const hookFetchData = async () => {
 	const featuresByClass = features
 	.reduce(groupByClasses(), {})
 
-	// Create a useful progression table index for this class.
-	const clericProgression = featuresByClass['cleric']
-	.map(fillProgression('cleric', 0, 5))
+	const classLevelNodes = document
+	.querySelectorAll('#class-leveling div div')
 
-	const wizardProgression = featuresByClass['wizard']
-	.map(fillProgression('wizard', 5, 20))
+	// Create a useful progression table index for each class.
+	// In the meantime, also collelct feature descriptions.
+	let fullProgression = []
+	let fullDescription = ''
+	for (const node of classLevelNodes) {
+		const classSlug = node['dataset']['class']
+		const firstLevel = parseInt(node.querySelector('.first-level')['value']) - 1
+		const finalLevel = parseInt(node.querySelector('.final-level')['value'])
 
-	const fullProgression = [...wizardProgression, ...clericProgression]
-	.reduce(mergeProgression())
+		// Here is the progression table.
+		const progression = features.map(fillProgression(classSlug, firstLevel, finalLevel))
+		const description = generateDescriptions(progression, classSlug, featuresByClass, featuresByName)
+
+		// Now combine it!
+		fullProgression.push(...(progression ?? []))
+		fullDescription += description
+	}
+	fullProgression = fullProgression.reduce(mergeProgression())
 
 	// Obtain the template element.
 	const templateEl = document.getElementById('character-data')
@@ -87,12 +143,7 @@ const hookFetchData = async () => {
 
 	// Classes and features are respectively returned
 	// once they are digested by this file.
-	templateEl.innerHTML += generateDescriptions(clericProgression, 'cleric', featuresByClass, featuresByName)
-	templateEl.innerHTML += generateDescriptions(wizardProgression, 'wizard', featuresByClass, featuresByName)
-
-	console.info('progression:')
-	console.dir(fullProgression)
-	***************************************************************************************/
+	templateEl.innerHTML += fullDescription
 }
 
 document
